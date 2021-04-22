2346ea0dd3276327c0c0e773b1831482b82f6da2
Lipzs <filipedias823@hotmail.com>
2021-04-22 20:34:02 -0300

    deploy adjusts

--- src/middlewares/cors.js
+++ src/middlewares/cors.js
@@ -14,7 +14,7 @@ function corsMd(req, res, next) {
 
   // Set to true if you need the website to include cookies in the requests sent
   // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Credentials', false);
 
   // Pass to next layer of middleware
   app.use(cors())
--- src/server.js
+++ src/server.js
@@ -8,6 +8,14 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+const options = {
+  origin: true,
+  methods: ["POST"],
+  credentials: true,
+  maxAge: 3600
+};
+app.options("/schedule", cors(options));
+
 app.use(routes);
 
 app.listen(process.env.PORT || 3333);
